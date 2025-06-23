8d0add794723188c5f2d3e3442fdd8261331d0c1
양시은 <mn07020@gmail.com>
2025-06-23 10:24:49 +0900

    초기 세팅:프론트엔드 및 백엔드 폴더 구성, theme.ts 추가

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary1: "#ECFAE5",
+    primary2: "#DDF6D2",
+    primary3: "#CAE8BD",
+    primary4: "#B0DB9C",
+
+    text: "#1F1F1F",
+    subText: "#4E4E4E",
+  },
+};
+
+export default theme;
