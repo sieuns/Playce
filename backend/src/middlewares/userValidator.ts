85fd9df6e3d386dc7668988ee3529902dde32ea7
jieun <jieunh0311@gmail.com>
2025-06-26 11:06:19 +0900

    Feat: 회원가입 유효성 검사 수정 (#9)

--- backend/src/middlewares/userValidator.ts
+++ backend/src/middlewares/userValidator.ts
@@ -8,13 +8,13 @@ export const JoinValidator = [
     .withMessage("이메일 형식이 올바르지 않습니다."),
   body("password")
     .notEmpty()
-    .isLength({ min: 3 })
-    .withMessage("비밀번호는 최소 3자 이상이어야 합니다."),
+    .isLength({ min: 6 })
+    .withMessage("비밀번호는 최소 6자 이상이어야 합니다."),
   body("name").notEmpty().withMessage("이름을 입력해주세요."),
   body("nickname").notEmpty().withMessage("닉네임을 입력해주세요."),
   body("phone")
     .notEmpty()
-    .matches(/^01[016789]-\d{3,4}-\d{4}$/)
-    .withMessage("전화번호 형식을 확인해주세요. 예: 010-1234-5678"),
+    .matches(/^01[016789]-?\d{3,4}-?\d{4}$/)
+    .withMessage("전화번호 형식을 확인해주세요. 예: 01012345678/010-1234-5678"),
   validate,
 ];
