dc3738de84ef71738668c8416d33dc25f37c875f
hwanga12 <ghkdrkdus1@naver.com>
2025-07-03 17:56:00 +0900

    Refactor: 정렬 기능 삭제 (#50)

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -20,8 +20,8 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: true,
-  logging: false,
+  synchronize: false,
+  logging: true,
   entities: [
     User,
     Store,
